b4e1778ab5e479ec5c187a210144e19cdf23332f
lucasabdouni <lucasabdouni@hotmail.com>
2023-04-17 17:28:44 -0300

    add immer and local storage

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -35,7 +35,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDiifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDiifference >= totalSeconds) {
